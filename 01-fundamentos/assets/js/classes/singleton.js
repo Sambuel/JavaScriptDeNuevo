5498dc8adf028a664c4089ba3fd129063c820798
Sambuel <123chamollito@gmail.com>
2024-11-25 17:23:08 -0600

    singleton

--- 01-fundamentos/assets/js/classes/singleton.js
+++ 01-fundamentos/assets/js/classes/singleton.js
@@ -31,3 +31,24 @@ console.log(`nombre en la instancia3 es: ${instancia3.nombre}`);
 
 
 
+class Singleton2{
+    static instancia;
+    heroe = ""
+
+    constructor(heroe = ""){
+        
+        if (Singleton2.instancia) {
+            return Singleton2.instancia
+        }
+
+        Singleton2.instancia = this
+        this.heroe = heroe;
+    };
+
+};
+
+const heroe1 = new Singleton2("kickass");
+console.log(heroe1);
+
+const heroe2 = new Singleton2("blackPanter");
+console.log(heroe2);
